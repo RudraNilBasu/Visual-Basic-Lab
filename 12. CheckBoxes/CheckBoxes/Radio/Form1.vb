9b18da1cbffe161921af8ef945d9c4a0542dc3a1
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-03-29 23:46:48 +0530

    Changed and deleted Stuff

---
+++ 12. CheckBoxes/CheckBoxes/Radio/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked = True Then
+            TextBox1.Text = "The Chosen Language is Java"
+        ElseIf RadioButton2.Checked = True Then
+            TextBox1.Text = "The Chosen Language is C++"
+        ElseIf RadioButton3.Checked = True Then
+            TextBox1.Text = "The Chosen Language is C"
+        ElseIf RadioButton4.Checked = True Then
+            TextBox1.Text = "The Chosen Language is Visual Basic"
+        ElseIf RadioButton5.Checked = True Then
+            TextBox1.Text = "The Chosen Language is Javascript"
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+    End Sub
+End Class
---
+++ 13. Matrix Multiplication/Matrix Multiplication/ArrayMul/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ListView3 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 286)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 17)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "First Matrix"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(223, 286)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Second Matrix"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(419, 279)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 28)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate Product"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(47, 48)
+        Me.ListView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(140, 212)
+        Me.ListView1.TabIndex = 6
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(227, 48)
+        Me.ListView2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(140, 212)
+        Me.ListView2.TabIndex = 7
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'ListView3
+        '
+        Me.ListView3.Location = New System.Drawing.Point(419, 48)
+        Me.ListView3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.ListView3.Name = "ListView3"
+        Me.ListView3.Size = New System.Drawing.Size(140, 212)
+        Me.ListView3.TabIndex = 8
+        Me.ListView3.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 401)
+        Me.Controls.Add(Me.ListView3)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ListView3 As System.Windows.Forms.ListView
+
+End Class
